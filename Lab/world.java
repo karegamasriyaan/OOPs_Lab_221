a076beb7ed2464c7fb0253719d9ae3711eb332b8
karegamasriyaan <karegamasriyaan@gmail.com>
2025-12-22 21:02:11 +0530

    Add files via upload

---
+++ Lab/world.java
@@ -0,0 +1,8 @@
+class world {
+    public static void main(String[] args){
+        System.out.print("The Country:");
+        System.out.println("USA");
+        System.out.print("The Capital:");
+        System.out.println("New York");
+    }
+}
